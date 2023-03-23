a102fb88c68de498e163cf5a62f215a7f3fc6403
Lucas Santos <lucassr614@gmail.com>
2023-03-23 11:06:29 -0300

    Update JDBC EX1

---
+++ JDBC/Ex02/src/src/db/DB.java
@@ -0,0 +1,48 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+public class DB {
+    private static Connection conexao = null;
+
+    public static Connection getConexao() {
+        if (conexao == null) {
+            try {
+                Properties props = loadProperties();
+                String url = props.getProperty("dburl");
+                conexao = DriverManager.getConnection(url, props);
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+
+        }
+        return conexao;
+    }
+
+    private static Properties loadProperties() {
+        String path = "C:/Users/lucas/OneDrive/Área de Trabalho/Cursos/Java Completo/JDBC/Ex01/src/src/db/db.properties";
+        try (FileInputStream fs = new FileInputStream(path)) {
+            Properties properties = new Properties();
+            properties.load(fs);
+            return properties;
+        } catch (IOException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    public static void fechaConexao() {
+        if (conexao != null) {
+            try {
+                conexao.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+}
