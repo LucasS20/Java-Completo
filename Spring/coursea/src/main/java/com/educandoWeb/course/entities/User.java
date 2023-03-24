4f491e3b3b2110df5411ea2647b6dfd1983c9c78
Lucas Santos <lucassr614@gmail.com>
2023-03-24 15:48:06 -0300

    Update banco h2

--- Spring/coursea/src/main/java/com/educandoWeb/course/entities/User.java
+++ Spring/coursea/src/main/java/com/educandoWeb/course/entities/User.java
@@ -1,11 +1,17 @@
 package com.educandoWeb.course.entities;
 
+import jakarta.persistence.*;
+import org.springframework.context.annotation.Primary;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
@@ -21,8 +27,7 @@ public class User implements Serializable {
         this.password = password;
     }
 
-    User() {
-
+    public User() {
     }
 
     //#endregion
